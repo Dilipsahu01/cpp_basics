0d85be0ac5504d1bb44433c6255a65af35345a82
Dilip Sahu <dilipsahuop@gmail.com>
2025-06-15 11:19:53 +0530

    practice6.cpp

---
+++ practice6.cpp
@@ -0,0 +1,20 @@
+//////                        majority element in the array  by mooriss /// answer will always exist in the given array
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+
+vector < int > v={1,2,3,1,1,2,2,1,4,1};
+int major=v[0],frequency=1;
+for (int i = 1;i< v.size();i++) {
+        if (v[i]==v[i-1]) {
+            frequency++;
+        } else frequency--;
+        if (frequency==0) {
+            major = v[i];
+            frequency=1;
+        }
+}
+cout << major;
+return 0;
+}
